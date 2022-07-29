1ad00399e3fa72d55a904c8a48ba43ca2507df36
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-07-29 11:55:42 -0500

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "default" {
+  count         = local.enabled ? 1 : 0
+  bucket        = local.bucket_name
+  force_destroy = var.force_destroy
+
+  dynamic "object_lock_configuration" {
+    for_each = var.object_lock_configuration != null ? [1] : []
+    content {
+      object_lock_enabled = "Enabled"
+    }
+  }
+}
